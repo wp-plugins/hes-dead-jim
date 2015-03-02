cb1113b50e993a3d78df14e4a8c3265dd733c8e4
kjodle <kjodle@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-02 01:39:11 +0000

    Improved appearance to use Open Sans font, enqueued stylesheets instead of adding css to <head>, verified works with current version of WordPress (4.1.1).
    git-svn-id: https://plugins.svn.wordpress.org/hes-dead-jim/trunk@1102920 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- hes_dead_jim.php
+++ hes_dead_jim.php
@@ -1,40 +1,67 @@
 <?php
-/**
- * @package Hes_Dead_Jim
- * @version 1.0
- */
 /*
 Plugin Name: He's Dead, Jim
-Plugin URI: http://blog.kjodle.net/
+Plugin URI: http://www.kjodle.net/wordpress/hes-dead-jim/
 Description: Do you like the idea behind Matt's "Hello, Dolly" plugin, but feel that it doesn't feed your inner nerd? Then here's "He's Dead, Jim" which has been shamelessly ported from Matt's plugin. The concept is the same, except that instead of lines from "Hello, Dolly" you get lines from Star Trek: The Original Series. 
 Most of the quotations were taken from: http://en.wikiquote.org/wiki/Star_Trek:_The_Original_Series
+Version: 1.1
 Author: Kenneth John Odle
-Version: 1.0
-Author URI: http://blog.kjodle.net/
+Author URI: http://techblog.kjodle.net
+Author Support: http://kjodle.info/support
+
+Copyright 2015 Kenneth John Odle
+© 2015 Kenneth John Odle
+
+Released under the GPL v.3, http://www.gnu.org/copyleft/gpl.html
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License, version 3, as 
+	published by the Free Software Foundation.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
+// Prevent this page from being loaded directly.
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+
+// Let's add our stylesheets:
+function d12_hdj_styles() {
+	wp_register_style( 'd12hdj_admin_css', plugins_url('/css/d12hdjstyle.css', __FILE__), false, '1.0.0' );
+	wp_enqueue_style( 'd12hdj_admin_css' );
+}
+add_action( 'admin_enqueue_scripts', 'd12_hdj_styles' );
+
+// Our set of Star Trek The Original Series lines:
 function hes_dead_jim_get_line() {
 	/** These are famous lines from "Star Trek: The Original Series" */
 	$lines = "He's dead, Jim.
 	They'll be no tribble at all.
 	Computer, compute to the last digit the value of pi.
 	Now this is a drink for a man.
-	You may find that having is not so pleasing a thing as wanting.<br />This is not logical, but it is often true.
+	You may find that having is not so pleasing a thing as wanting.This is not logical, but it is often true.
 	The best diplomat that I know is a fully-loaded phaser bank.
-	I signed aboard this ship to practice medicine, <br />not to have my atoms scattered back and forth across space by this gadget.
+	I signed aboard this ship to practice medicine, not to have my atoms scattered back and forth across space by this gadget.
 	You will die of suffocation, in the icy cold of space.
 	Shut-up, Spock! We're rescuing you!
 	Evil does seek to maintain power by suppressing the truth.
 	How many more men must die before you two begin to act like military men instead of fools?
 	Brain and brain!  What is brain?
 	Random chance seems to have operated in our favor.
-	Mr. Spock, the women on your planet are logical.<br />That's the only planet in the galaxy that can make that claim.
+	Mr. Spock, the women on your planet are logical.That's the only planet in the galaxy that can make that claim.
 	I'm a doctor, not a bricklayer.
 	My dear girl, I'm a doctor. When I peek, it is in the line of duty.
 	I'm a doctor, not a mechanic.
 	I'm a doctor, not an engineer.
 	I'm a doctor, not a coal miner.
-	Instruments register only those things they're designed to register.<br />Space still contains infinite unknowns.
+	Instruments register only those things they're designed to register.Space still contains infinite unknowns.
+	Infinite diversity in infinite combinations.
 	Our spectro-readings showed no contamination, no unusual elements present.
 	Don't risk your life on a theory!
 	Are you aware it's the captain's guts you're analyzing?
@@ -43,8 +70,8 @@ function hes_dead_jim_get_line() {
 	We humans are full of unpredictable emotions that logic cannot solve.
 	You and I are of a kind. In a different reality, I could have called you friend.
 	Life and death are seldom logical.
-	Oh, how absolutely typical of your species!<br />You don't understand something so you become fearful.
-	I object to intellect without discipline.<br />I object to power without constructive purpose.
+	Oh, how absolutely typical of your species!You don't understand something so you become fearful.
+	I object to intellect without discipline.I object to power without constructive purpose.
 	I made an error in my computations.
 	Oh, I like them fine, but a computer takes less space.
 	Your statement is irrelevant.
@@ -57,7 +84,7 @@ function hes_dead_jim_get_line() {
 	You can't evaluate a man by logic alone.
 	Immortality consists largely of boredom.
 	I'm a doctor, not an escalator.
-    In the strict scientific sense, Doctor, we all feed on death, even vegetarians.
+	In the strict scientific sense, Doctor, we all feed on death, even vegetarians.
 	Brace yourself. The area of penetration will no doubt be sensitive.
 	I'm trying to thank you, you pointy-eared hobgoblin!
 	It would be illogical to assume that all conditions remain stable.
@@ -67,7 +94,7 @@ function hes_dead_jim_get_line() {
 	Change is the essential process of all existence.
 	Death, when unnecessary, is a tragic thing.
 	Violence in reality is quite different from theory.
-	I am pleased to see that we have differences.<br />May we together become greater than the sum of both of us.
+	I am pleased to see that we have differences.May we together become greater than the sum of both of us.
 	History tends to exaggerate.
 	There is nothing good in war except its ending. 
 	Youth doesn't excuse everything.
@@ -82,44 +109,18 @@ function hes_dead_jim_get_line() {
 }
 
 // Echo the chosen line and make sure it stays out of the way of "Hello, Dolly" 
-function hes_dead_jim() {
+function d12_hes_dead_jim() {
 	$chosen = hes_dead_jim_get_line();
+	/*
 	// If "Hello, Dolly" is activated, move the output of this plugin to a new line
-	if (is_plugin_active('hello.php')) {
-		echo "<div style=\"clear:both;\"></div>";
+	if ( function_exists( 'hello_dolly_get_lyric' ) ) {
+		echo '<div class="d12hdjclear"></div>';
 	}
+	*/
 	echo "<p id='hesdeadjim'>$chosen</p>";
 }
 
 // Now we set that function up to execute when the admin_notices action is called
-add_action( 'admin_notices', 'hes_dead_jim' );
-
-// We need some CSS to position the paragraph
-function dead_jim_css() {
-	// This makes sure that the positioning is also good for right-to-left languages
-	$x = is_rtl() ? 'left' : 'right';
-
-	echo "
-	<style type='text/css'>
-	#hesdeadjim {
-		float: $x;
-		padding: 5px 15px 2px 15px;
-		padding-left: 15px;
-		padding-top: 5px;
-		padding-bottom: 2px;		
-		margin: 0 12px 3px 0;
-		font-size: 11px;
-		font-family:\"Lucida Console\", monospace;
-		text-align: right;
-		background: #003;
-		border: solid 4px #ffd;
-		color: #ffffd9;
-		border-radius: 9px;	
-	}
-	</style>
-	";
-}
-
-add_action( 'admin_head', 'dead_jim_css' );
+add_action( 'admin_notices', 'd12_hes_dead_jim' );
 
 ?>
--- readme.txt
+++ readme.txt
@@ -2,17 +2,19 @@
 Contributors: kjodle
 Donate link: http://blog.kjodle.net/2012/07/26/hes-dead-jim-plugin-for-wordpress/
 Tags: 
-Requires at least: 2.0.2
-Tested up to: 3.4.1
-Stable tag: 1.0
-License: GPLv2 or later
-License URI: http://www.gnu.org/licenses/gpl-2.0.html
+Requires at least: 3.0
+Tested up to: 4.1.1
+Stable tag: 1.1
+License: GPLv3 or later
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
 
 Like "Hello Dolly" from Matt, "He's Dead, Him" produces random lines from Star Trek: The Original Series.
 
 == Description ==
 
 Do you like the idea behind Matt's "Hello, Dolly" plugin, but feel that it doesn't feed your inner nerd? Then here's "He's Dead, Jim" which has been shamelessly ported from Matt's plugin. The concept is the same, except that instead of lines from "Hello, Dolly" you get lines from Star Trek: The Original Series. There are no settings, just install it, activate it, and enjoy.
+
+For more information, visit the [plugin's homepage] (http://www.kjodle.net/wordpress/hes-dead-jim/). 
 
 == Installation ==
 
@@ -23,22 +25,26 @@ Do you like the idea behind Matt's "Hello, Dolly" plugin, but feel that it doesn
 
 = What happens if "Hello Dolly" is still activated? =
 
-Normally, "He's Dead, Jim" will output its text where "Hello Dolly" does. If "Hello Dolly" is activated, "He's Dead Jim" will output its text *underneath* the "Screen Options" and "Help" tabs.
+Normally, "He's Dead, Jim" will output its text where "Hello Dolly" does. If "Hello Dolly" is activated, "He's Dead Jim" will output its text *to the left* of "Hello Dolly"'s output.
 
-== Screenshots ==
+= What happens if "Make It So" is also activated? = 
 
-1. "He's Dead, Jim" in action. 
-2. Appearance when "Hello Dolly" is activated. 
+"He's Dead, Jim" will appear in a box to the left of the screen, and "Make It So" will appear in a box to the right of that.
 
 == Changelog ==
 
 = 1.0 =
 * Initial release.
 
+= 1.1 =
+* Added the css to a separate stylesheet, saving on server resources and enabling default styles to be more easily overridden in a child theme.
+* 
+
 == Upgrade Notice ==
 
-= 1.0 =
+= 1.1 =
+Version 1.1 looks better with the current WordPress styling, as it also makes use of the "Open Sans" font. It also caches the default styles, saving server resources.
 
 == More Info ==
 
-You can read more about this plugin [here](http://blog.kjodle.net/2012/07/26/hes-dead-jim-plugin-for-wordpress/). You can read about the history behind it (and see some interesting screen shots) [here](http://blog.kjodle.net/2012/07/24/in-which-i-kind-of-learn-how-to-create-a-wordpress-plugin/)
+You can read more about this plugin [here](http://techblog.kjodle.net/2012/07/26/hes-dead-jim-plugin-for-wordpress/). You can read about the history behind it (and see some interesting screen shots) [here](http://techblog.kjodle.net/2012/07/24/in-which-i-kind-of-learn-how-to-create-a-wordpress-plugin/)
